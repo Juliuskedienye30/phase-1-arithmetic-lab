5e7b1a91d5d8fee221e9fad60590febf0bc8b1e3
Julius Kedienye <juliuskedienye61@gmail.com>
2025-06-06 21:03:43 +0300

    assignment on arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// index.js
 
-// Write your code here
+// Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // 62
+
+// Random
+let random = Math.floor(Math.random() * 100) + 1; // any integer > 0
+
+// Modulus
+let num3 = 20;
+let num4 = 8;
+let mod = num3 % num4; // 4
+
+// Max
+let max = Math.max(1, 2, 3, 4, 5, 20); // 20
